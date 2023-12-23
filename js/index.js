7ce7b6458291c53471d2504ad6aa14e1c495f733
Adnan <adnanfauzanm23@gmail.com>
2023-12-24 02:37:34 +0700

    Solve fitur

--- js/index.js
+++ js/index.js
@@ -2,20 +2,33 @@ const notif = document.getElementById("alert")
 const popupCv = document.getElementById("popup-cv")
 const popupCertificate = document.getElementById("main-contain")
 const cardCertif = document.getElementById("card-certificate")
-
+const header = document.getElementById("header")
+const medsoc = document.getElementById("medsoc")
 
 // POPUP OPEN CV
 function openCv(){
 if(popupCv){
   popupCv.style.visibility = "visible"
 }
+if(header){
+  header.style.zIndex = "0"
+}
+if(medsoc){
+  medsoc.style.zIndex = "-10"
+}
   document.body.classList.add("disableScroll")
 }
 
-// CLOSE OPEN CV
+// CLOSE  CLOSE CV
 function closeCv(){
   if(popupCv){
     popupCv.style.visibility = "hidden"
+  }
+  if(header){
+    header.style.zIndex = "100"
+  }
+  if(medsoc){
+    medsoc.style.zIndex = "2"
   }
   document.body.classList.remove("disableScroll")
 }
@@ -44,6 +57,12 @@ function openCertificate(){
   if(popupCertificate){
     popupCertificate.style.visibility = "visible"
   }
+  if(header){
+    header.style.zIndex = "0"
+  }
+  if(medsoc){
+    medsoc.style.zIndex = "-10"
+  }
   document.body.classList.add("disableScroll")
 }
 
@@ -56,5 +75,22 @@ function closeCertificate(){
   if(cardCertif){
     cardCertif.style.display = "none"
   }
+  if(header){
+    header.style.zIndex = "100"
+  }
+  if(medsoc){
+    medsoc.style.zIndex = "2"
+  }
+  
   document.body.classList.remove("disableScroll")
 }
+
+
+// HEADER NAVBAR
+window.addEventListener("scroll", function (){
+  if(this.window.scrollY > 0){
+    header.classList.add("headerScroled")
+  } else {
+    header.classList.remove("headerScroled")
+  }
+})
