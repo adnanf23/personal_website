eb11937067ce8353601776492eaccce7f623651a
Adnan <adnanfauzanm23@gmail.com>
2024-03-30 00:52:45 +0700

    Responsif Display

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
 const navMenu = document.getElementById("navbar");
 const openMenu = document.getElementById("menu");
 const closeMenu = document.getElementById("close");
+const header = document.getElementById("header");
+
+window.addEventListener("scroll", function (){
+  if (window.scrollY > 10) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled")
+  }   
+})
 
 
 // Open Navbar
@@ -10,5 +19,6 @@ openMenu.addEventListener("click", function () {
 
 // Close Nabara
 closeMenu.addEventListener("click", function () {
-  navMenu.style.left = "-200px";
+  navMenu.style.left = "-300px";
 })
+
